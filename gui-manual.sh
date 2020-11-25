8a3725650860701f65b7ddafa8d844972287a13f
terminal-guy <vishaladhithya19@gmail.com>
2020-11-26 01:50:45 +0530

    adding adding a new man pager with pdf reader

---
+++ gui-manual.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+manual=$(apropos -s ${SECTION:-''} ${@:-.} | \
+    grep -v -E '^.+ \(0\)' |\
+    awk '{print $2 "    " $1}' | \
+    sort | \
+    dmenu -l 20 -c -bw 3 -g 2 -i -p "  : " | \
+    sed -E 's/^\((.+)\)/\1/')
+
+if [ -z "$MANUAL" ]; then
+  man -T${FORMAT:-pdf} $manual | ${READER:-zathura -}
+fi
