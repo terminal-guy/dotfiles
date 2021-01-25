74cc5e53a163fc56f894165a47ac358c47e9eae0
terminal-guy <vishaladhithya19@gmail.com>
2021-01-25 22:01:09 +0530

    making everything work

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,8 +1,123 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; ██╗   ██╗ █████╗
+;; ██║   ██║██╔══██╗ Vishal Adhithya (VA)
+;; ██║   ██║███████║ https://github.com/terminal-guy/ (GITHUB  )
+;; ╚██╗ ██╔╝██╔══██║
+;;  ╚████╔╝ ██║  ██║
+;;   ╚═══╝  ╚═╝  ╚═╝
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
 
+;;  ███╗   ███╗██╗   ██╗██╗  ██╗███████╗
+;;  ████╗ ████║██║   ██║██║  ██║██╔════╝
+;;  ██╔████╔██║██║   ██║███████║█████╗
+;;  ██║╚██╔╝██║██║   ██║╚════██║██╔══╝
+;;  ██║ ╚═╝ ██║╚██████╔╝     ██║███████╗
+;;  ╚═╝     ╚═╝ ╚═════╝      ╚═╝╚══════╝
+
+
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+
+(setq user-full-name "Vishal Adhithya"
+      user-mail-address "vishaladhithya19@gmail.com"
+      ;; I have my mbsyncrc in a different folder on my system, to keep it separate from the
+      ;; mbsyncrc available publicly in my dotfiles. You MUST edit the following line.
+      ;; Be sure that the following command is: "mbsync -c ~/.config/mu4e/mbsyncrc -a"
+      mu4e-get-mail-command "mbsync -a"
+      mu4e-update-interval  300
+      mu4e-main-buffer-hide-personal-addresses t
+      message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials '(("smtp.1and1.com" 587 nil nil)))
+
+
+
+;; ██╗   ██╗██╗    ████████╗ ██████╗  ██████╗  ██████╗ ██╗     ███████╗███████╗
+;; ██║   ██║██║    ╚══██╔══╝██╔═══██╗██╔════╝ ██╔════╝ ██║     ██╔════╝██╔════╝
+;; ██║   ██║██║       ██║   ██║   ██║██║  ███╗██║  ███╗██║     █████╗  ███████╗
+;; ██║   ██║██║       ██║   ██║   ██║██║   ██║██║   ██║██║     ██╔══╝  ╚════██║
+;; ╚██████╔╝██║       ██║   ╚██████╔╝╚██████╔╝╚██████╔╝███████╗███████╗███████║
+;;  ╚═════╝ ╚═╝       ╚═╝    ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝
+
+(setq doom-font (font-spec :family "Mononoki Nerd Font" :size 15)
+      doom-variable-pitch-font (font-spec :family "JetBrains mono Medium" :size 15)
+      doom-big-font (font-spec :family "Mononoki Nerd Font" :size 24))
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+
+
+(setq doom-modeline-modal-icon nil)
+(setq evil-echo-state nil)
+(setq evil-normal-state-tag   (propertize "[Normal]" 'face '((:background "green" :foreground "black")))
+      evil-emacs-state-tag    (propertize "[Emacs]" 'face '((:background "orange" :foreground "black")))
+      evil-insert-state-tag   (propertize "[Insert]" 'face '((:background "red") :foreground "white"))
+      evil-motion-state-tag   (propertize "[Motion]" 'face '((:background "blue") :foreground "white"))
+      evil-visual-state-tag   (propertize "[Visual]" 'face '((:background "grey80" :foreground "black")))
+      evil-operator-state-tag (propertize "[Operator]" 'face '((:background "purple"))))
+
+
+(require 'unicode-fonts)
+(unicode-fonts-setup)
+
+(setq doom-theme 'doom-city-lights)
+
+
+
+;; ██████╗ ██████╗  ██████╗     ███╗   ███╗ ██████╗ ██████╗ ███████╗
+;; ██╔═══██╗██╔══██╗██╔════╝     ████╗ ████║██╔═══██╗██╔══██╗██╔════╝
+;; ██║   ██║██████╔╝██║  ███╗    ██╔████╔██║██║   ██║██║  ██║█████╗
+;; ██║   ██║██╔══██╗██║   ██║    ██║╚██╔╝██║██║   ██║██║  ██║██╔══╝
+;; ╚██████╔╝██║  ██║╚██████╔╝    ██║ ╚═╝ ██║╚██████╔╝██████╔╝███████╗
+
+(after! org
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(setq org-directory "~/Org/*"
+      org-agenda-files '("~/Org/agenda.org")
+      org-ellipsis " ▼ "
+
+      org-hide-emphasis-markers t
+)
+ (setq org-log-done 'time)
+)
+
+(setq display-line-numbers-type t)
+
+
+;; ██╗     ███████╗██████╗
+;; ██║     ██╔════╝██╔══██╗
+;; ██║     ███████╗██████╔╝
+;; ██║     ╚════██║██╔═══╝
+;; ███████╗███████║██║
+;; ╚══════╝╚══════╝╚═╝
+
+
+(use-package lsp-mode :ensure t)
+(use-package lsp-dart
+  :ensure t
+  :hook (dart-mode . lsp))
+
+;; Optional packages
+(use-package projectile :ensure t) ;; project management
+(use-package yasnippet
+  :ensure t
+  :config (yas-global-mode)) ;; snipets
+(use-package lsp-ui :ensure t) ;; UI for LSP
+(use-package company :ensure t) ;; Auto-complete
+
+;; Optional Flutter packages
+(use-package hover :ensure t) ;; run app from desktop without emulator
+
+
+
+;; ███████╗██╗  ██╗██╗    ██╗███╗   ███╗
+;; ██╔════╝╚██╗██╔╝██║    ██║████╗ ████║
+;; █████╗   ╚███╔╝ ██║ █╗ ██║██╔████╔██║
+;; ██╔══╝   ██╔██╗ ██║███╗██║██║╚██╔╝██║
+;; ███████╗██╔╝ ██╗╚███╔███╔╝██║ ╚═╝ ██║
+;; ╚══════╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚═╝     ╚═╝
 
 (require 'exwm)
 (require 'exwm-config)
@@ -61,121 +176,5 @@
 
 
 
-  ;; Set the wallpaper after changing the resolution
-  (efs/set-wallpaper)
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-
-
-
-;;  ███╗   ███╗██╗   ██╗██╗  ██╗███████╗
-;;  ████╗ ████║██║   ██║██║  ██║██╔════╝
-;;  ██╔████╔██║██║   ██║███████║█████╗
-;;  ██║╚██╔╝██║██║   ██║╚════██║██╔══╝
-;;  ██║ ╚═╝ ██║╚██████╔╝     ██║███████╗
-;;  ╚═╝     ╚═╝ ╚═════╝      ╚═╝╚══════╝
-
-(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
-
-(setq user-full-name "Vishal Adhithya"
-      user-mail-address "vishaladhithya19@gmail.com"
-      ;; I have my mbsyncrc in a different folder on my system, to keep it separate from the
-      ;; mbsyncrc available publicly in my dotfiles. You MUST edit the following line.
-      ;; Be sure that the following command is: "mbsync -c ~/.config/mu4e/mbsyncrc -a"
-      mu4e-get-mail-command "mbsync -a"
-      mu4e-update-interval  300
-      mu4e-main-buffer-hide-personal-addresses t
-      message-send-mail-function 'smtpmail-send-it
-      starttls-use-gnutls t
-      smtpmail-starttls-credentials '(("smtp.1and1.com" 587 nil nil)))
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-(setq doom-font (font-spec :family "Mononoki Nerd Font" :size 15)
-      doom-variable-pitch-font (font-spec :family "JetBrains mono Medium" :size 15)
-      doom-big-font (font-spec :family "Mononoki Nerd Font" :size 24))
-(after! doom-themes
-  (setq doom-themes-enable-bold t
-        doom-themes-enable-italic t))
-(custom-set-faces!
-  '(font-lock-comment-face :slant italic)
-  '(font-lock-keyword-face :slant italic))
-
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-city-lights)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(after! org
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(setq org-directory "~/Org/*"
-      org-agenda-files '("~/Org/agenda.org")
-      org-ellipsis " ▼ "
-
-      org-hide-emphasis-markers t
-)
- (setq org-log-done 'time)
-)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-
-(use-package lsp-mode :ensure t)
-(use-package lsp-dart
-  :ensure t
-  :hook (dart-mode . lsp))
-
-;; Optional packages
-(use-package projectile :ensure t) ;; project management
-(use-package yasnippet
-  :ensure t
-  :config (yas-global-mode)) ;; snipets
-(use-package lsp-ui :ensure t) ;; UI for LSP
-(use-package company :ensure t) ;; Auto-complete
-
-;; Optional Flutter packages
-(use-package hover :ensure t) ;; run app from desktop without emulator
-
-
-(setq doom-modeline-modal-icon nil)
-(setq evil-echo-state nil)
-(setq evil-normal-state-tag   (propertize "[Normal]" 'face '((:background "green" :foreground "black")))
-      evil-emacs-state-tag    (propertize "[Emacs]" 'face '((:background "orange" :foreground "black")))
-      evil-insert-state-tag   (propertize "[Insert]" 'face '((:background "red") :foreground "white"))
-      evil-motion-state-tag   (propertize "[Motion]" 'face '((:background "blue") :foreground "white"))
-      evil-visual-state-tag   (propertize "[Visual]" 'face '((:background "grey80" :foreground "black")))
-      evil-operator-state-tag (propertize "[Operator]" 'face '((:background "purple"))))
-
+;; Set the wallpaper after changing the resolution
+(efs/set-wallpaper)
---
+++ .doom.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default))
+ '(package-selected-packages
+   '(unicode-fonts vterm powerline pkg-info pandoc org-timeline markdown-preview-mode grip-mode exwm dart-mode dap-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic)))))
